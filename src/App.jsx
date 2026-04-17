4f437676b7b9d212c04b2a0e8369517190a3b44d
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-17 22:27:17 +0530

    [Frontend] routing updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import Home from '../Pages/Home'
+import Contact from '../Pages/Contact'
+import About from '../Pages/About'
 import SmoothScroll from '../Components/SmoothScroll'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-    <SmoothScroll>
     <div className='bg-white'>
-         <Home/>
+      <BrowserRouter>
+        <SmoothScroll>
+          <Routes> 
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </SmoothScroll>
+      </BrowserRouter>
     </div>
-    </SmoothScroll>
-    </>
   )
 }
 
