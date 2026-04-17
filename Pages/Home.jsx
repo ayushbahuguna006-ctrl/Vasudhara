083b6bd75eb5ee54f032b8b7a1a276fe1339abcc
Ayush <ayushbahuguna006@gmail.com>
2026-04-17 22:03:15 +0530

    [Frontend] UX Updation

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Herosection1 from '../Components/Herosection1'
 import Herosection2 from '../Components/Herosection2'
-import SmoothScroll from '../Components/SmoothScroll'
+
 
 function App() {
   return (
-    <SmoothScroll>
+    <>
       <Herosection1 />
       <Herosection2 />
-    </SmoothScroll>
+    </>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Home from '../Pages/Home'
+import SmoothScroll from '../Components/SmoothScroll'
 
 function App() {
   return (
     <>
+    <SmoothScroll>
     <div className='bg-white'>
          <Home/>
     </div>
+    </SmoothScroll>
     </>
   )
 }
