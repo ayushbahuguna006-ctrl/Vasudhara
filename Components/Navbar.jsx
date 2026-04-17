5edfe747efa3bd070819a29a578a21d11ee075e2
Ayush <ayushbahuguna006@gmail.com>
2026-04-17 12:31:57 +0530

    [Frontend] navbar updated

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-      <nav className='flex justify-center gap-2 md:gap-10 p-2 font-bold mt-5 bg-gradient-to-r from-[#1a1a1a]/80 via-[#2a2a2a]/70 to-[#1a1a1a]/80 
+      <nav className='flex justify-around gap-2 md:gap-10 p-2 font-bold mt-5 bg-gradient-to-r from-[#1a1a1a]/80 via-[#2a2a2a]/70 to-[#1a1a1a]/80 
       backdrop-blur-md rounded-full md:mx-[20vw] border border-white/10  text-white shadow-lg '>
 
         <p className='hidden md:block translate-y-3'>Home</p>
